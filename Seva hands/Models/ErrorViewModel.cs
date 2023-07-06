71427158fa674863cc48c38480685de29c2d2538
ST10084431 <ST10084431@vcconnect.edu.za>
2023-07-06 19:23:04 +0300

    Add project files.

---
+++ Seva hands/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Seva_hands.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
